fab4971f2b1433e01e5c707ac92b365b1a756b6f
Arslan <akhurram@cinnova.com>
2025-05-26 10:31:18 +0500

    first commit

---
+++ webserver.py
@@ -0,0 +1,22 @@
+# For Webserver - Flask
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+@app.route('/lower', methods=['GET'])
+def lower():
+    return render_template ('lower.html')
+
+
+@app.route('/upper', methods=['GET'])
+def upper():
+    return render_template ('upper.html')
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template ('index.html')
+
+
+app.run(host="127.0.0.1", port=80,debug=True)
+
+
+
