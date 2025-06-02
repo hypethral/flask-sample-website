8c6cda0025ee1aed01a7056250e7b5ad9a71fcc6
Arslan <akhurram@cinnova.com>
2025-06-02 18:39:03 +0500

    cb7ec255cfc143ad4847a2682539e25f

--- webserver.py
+++ webserver.py
@@ -4,9 +4,10 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 @app.route('/lower', methods=['GET'])
 def lower():
+    # test = "cb7ec255cfc143ad4847a2682539e25f"
     return render_template ('lower.html')
 
-test = "cb7ec255cfc143ad4847a2682539e25f"
+
 
 @app.route('/upper', methods=['GET'])
 def upper():
